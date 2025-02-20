808ee94ee88ba944ae030b933c0307cdaeeead2f
xupin <74996124+Xp1307@users.noreply.github.com>
2025-02-20 10:09:44 +0800

    Update Main_twibot22_100w_RGCN_Backbone_Concat_DynCrossEntropy.py

--- Main_twibot22_100w_RGCN_Backbone_Concat_DynCrossEntropy.py
+++ Main_twibot22_100w_RGCN_Backbone_Concat_DynCrossEntropy.py
@@ -49,14 +49,12 @@ class BotRGCN_Trainer(object):
         self.test_label_list = []
         self.test_pred_list = []
 
-        #! 创建分类器(就是一个MLP), 这个也要放入GPU中
         self.concat_classifier = nn.Sequential(
             nn.Linear(2*self.args.hidden_dim, self.args.hidden_dim),
             nn.LeakyReLU(),
             nn.Linear(self.args.hidden_dim, self.args.num_classes))
         self.concat_classifier.to('cuda:'+str(args.device_id))
 
-        #! 创建可训练的权重参数
         self.weight1 = torch.nn.Parameter(torch.tensor(1, dtype=torch.float32, requires_grad=True).to('cuda:'+str(args.device_id)))
         self.weight2 = torch.nn.Parameter(torch.tensor(1, dtype=torch.float32, requires_grad=True).to('cuda:'+str(args.device_id)))
         
@@ -71,12 +69,6 @@ class BotRGCN_Trainer(object):
 
     #! 开始训练
     def concat_train(self, logger=None):
-        r'''
-            训练器进行训练, 并返回结果(Acc, Pre, F1等), 分别返回验证集、测试集结果
-            返回的都是最小损失对应的标签分布和预测分布
-            - self.val_label_list[min_loss_index[0]], self.val_pred_list[min_loss_index[0]]
-            - self.test_label_list[min_loss_index[0]], self.test_pred_list[min_loss_index[0]]           
-        '''
         #! 创建了两个 bot_rgcn, 同时进行优化
         optimizer = torch.optim.AdamW(list(self.botrgcn.parameters())+list(self.raw_botrgcn.parameters()),
                                             lr=args.lr,
@@ -88,40 +80,12 @@ class BotRGCN_Trainer(object):
         val_losses = []
         test_accs = []
 
-        # #@ 创建交叉熵损失函数，带权重，设置固定权重
-        # class_weights = torch.tensor([0.5, 2.0], dtype=torch.float).to('cuda:{}'.format(self.args.device_id))
-        # loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights)
-        
-        #@ 创建交叉熵损失函数，带权重，设置固定权重
-        #@ 这里改成动态的
-        # labels = self.data.y[self.data.train_mask]
-        # class_sample_count = torch.bincount(labels)
-        # #@ 计算每个类别的权重 (可以是样本数量的倒数)
-        # class_weights = torch.tensor([each/class_sample_count.sum() for each in reversed(class_sample_count)], 
-        #                             dtype=torch.float).to('cuda:{}'.format(self.args.device_id))
-        loss_fn = torch.nn.CrossEntropyLoss()
-        
-        # #@ 创建交叉熵损失函数，带权重，设置固定权重
-        # #@ 这里改成动态的
-        # labels = self.data.y[self.data.train_mask]
-        # class_sample_count = torch.bincount(labels)
-        # #@ 计算每个类别的权重 (可以是样本数量的倒数)
-        # class_weights = torch.tensor([each/class_sample_count.sum() for each in reversed(class_sample_count)], 
-        #                             dtype=torch.float)
-        # class_weights = torch.tensor([1+class_weights[0]*(class_weights[0]-class_weights[1]),
-        #                               1+class_weights[1]*(class_weights[1]-class_weights[0])], 
-        #                             dtype=torch.float).to('cuda:{}'.format(self.args.device_id))
-        # loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights)
-               
-        # print('='*90)
-        # print('labels = self.data.y[self.data.train_mask]')
-        # print('class_sample_count = torch.bincount(labels)')
-        # print(class_sample_count)
-        # print('''torch.tensor([each/class_sample_count.sum() for each in reversed(class_sample_count)], 
-        #                             dtype=torch.float).to('cuda:0')''')
-        # print(class_weights)
-        # print('loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights)')
-        # print('='*90)
+        labels = self.data.y[self.data.train_mask]
+        class_sample_count = torch.bincount(labels)
+        class_weights = torch.tensor([each/class_sample_count.sum() for each in reversed(class_sample_count)], 
+                                    dtype=torch.float).to('cuda:{}'.format(self.args.device_id))
+        loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights)
+
                 
         for epoch in range(args.epochs):
             #! 进行训练
@@ -130,31 +94,20 @@ class BotRGCN_Trainer(object):
             
             optimizer.zero_grad()
             
-            #! 获得两个通道的 embedding
             out_embedding = self.botrgcn(self.data)
             raw_embedding = self.raw_botrgcn(self.raw_data)
 
-            # 设置权重
-            # total_weight = self.weight1 + self.weight2
-            # normalized_weight1 = self.weight1 / total_weight
-            # normalized_weight2 = self.weight2 / total_weight
-            # train_out = self.concat_classifier(torch.cat((
-            #     torch.mul(out_embedding[self.data.train_mask], normalized_weight1),
-            #     torch.mul(raw_embedding[self.raw_data.train_mask], normalized_weight2)
-            # ), dim=1))
             train_out = self.concat_classifier(torch.cat((
                 out_embedding[self.data.train_mask],
                 raw_embedding[self.raw_data.train_mask]
                 ), dim=1))
             
-            # loss = F.cross_entropy(train_out, self.data.y[self.data.train_mask])
             loss = loss_fn(train_out, self.data.y[self.data.train_mask])
             
             loss.backward()
             optimizer.step()
             scheduler.step()
 
-            #! 进行验证, 这里的 eval() 是训练器本身自带的
             val_acc, val_loss, test_acc, _ = self.eval()            
 
             self.organize_val_log(val_loss, val_acc, epoch)
@@ -164,12 +117,9 @@ class BotRGCN_Trainer(object):
             if self.patience > args.patience:
                 break
 
-        #! 获得测试结果
         test_results = self.get_test_results()
         test_results = np.array(test_results)
         
-        #! 选择验证集上loss最小的 self.save_top_k 个结果打印, 这里是根据 val_loss 选择的, 我能不能换成 val_acc 为准?
-        #! argsort() 返回元素按从小到大排序后的索引
         val_losses = np.array(val_losses)
         test_accs = np.array(test_accs)
         
@@ -199,23 +149,18 @@ class BotRGCN_Trainer(object):
                 'epoch: %d, test_acc: %.4f, test_f1: %.4f, test_recall: %.4f, test_precision: %.4f, val_acc: %.4f'
                 % (i+1, test_results[i][0], test_results[i][1],
                     test_results[i][2], test_results[i][3], test_results[i][4]))
-            
-        #! 返回 val_label|pred, test_label|pred, 选取验证集损失值最小的对应的测试集的各项指标
-        # return self.val_label_list[min_loss_index[0]], self.val_pred_list[min_loss_index[0]], \
-        #         self.test_label_list[min_loss_index[0]], self.test_pred_list[min_loss_index[0]]  
+             
         return self.val_label_list[min_test_accs_index[self.save_top_k-1]], self.val_pred_list[min_test_accs_index[self.save_top_k-1]], \
                 self.test_label_list[min_test_accs_index[self.save_top_k-1]], self.test_pred_list[min_test_accs_index[self.save_top_k-1]]    
                      
     def organize_val_log(self, val_loss, val_acc, epoch):
-        #! 如果损失变小了
         if val_loss < self.best_loss:
             self.best_loss_acc = val_acc
             self.best_loss = val_loss
             self.best_loss_epoch = epoch
             self.patience = 0
         else:
-            self.patience += 1          #! 损失没有减小
-        #! 如果验证集准确率大了
+            self.patience += 1
         if val_acc > self.best_acc:
             self.best_acc = val_acc
             self.best_acc_loss = val_loss
@@ -243,10 +188,8 @@ class BotRGCN_Trainer(object):
         val_label = self.data.y[self.data.val_mask].cpu().numpy()
         val_pred = torch.argmax(val_out, dim=1).cpu().numpy()
         
-        #! 获得验证集的准确率
         val_acc = accuracy_score(val_label, val_pred)
         
-        #! 获得测试集结果
         test_out = self.concat_classifier(torch.cat((
                 out_embedding[self.data.test_mask],
                 raw_embedding[self.raw_data.test_mask]
@@ -255,18 +198,11 @@ class BotRGCN_Trainer(object):
         test_label = self.data.y[self.data.test_mask].cpu().numpy()
         test_pred = torch.argmax(test_out, dim=1).cpu().numpy()
         
-        #! 获得准确率、F1值、召回率、精确率
         test_acc = accuracy_score(test_label, test_pred)
         test_f1 = f1_score(test_label, test_pred, zero_division=0)
         test_recall = recall_score(test_label, test_pred, zero_division=0)
-        #@ 如果分母为0，那么把 Precision 的值设为 0
         test_precision = precision_score(test_label, test_pred, zero_division=0) 
-         
-        # #! 超过basline的分数
-        # if test_acc > 0.869:
-        #     print('large test_acc: ', test_acc)
         
-        #! 记录结果
         self.test_results.append(
             [test_acc, test_f1, test_recall, test_precision, val_acc])
         
@@ -316,15 +252,12 @@ if __name__ == '__main__':
                         default=0.1)  # subgraph self-supervised loss
     parser.add_argument('--num_blocks', type=int, default=2)
     parser.add_argument('--num_convs', type=int, default=3)
-    
-    #! edge dropout ratee
+
     parser.add_argument('--pe', type=float, default=0.2)  # edge dropout rate
     parser.add_argument('--pf', type=float, default=0.2)
 
     parser.add_argument('--seed', type=int, default=42, help='seed')
     parser.add_argument('--batch_size', default=5000, type=int)
-    
-    #! 下面是优化器的参数
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--weight_decay', type=float, default=3e-3)
     
@@ -344,11 +277,10 @@ if __name__ == '__main__':
     print(formated_args)
     
     device=torch.device('cuda:{}'.format(args.device_id))
-    #! 读取异质图子图列表
     dataset_name = args.dataset_name
-    hetero_graph_list = torch.load('/data3/xupin/0_UNName/data/way_2/twibot22_100w/'+dataset_name+'_subgraphlist_hetero_to_type_1_node_hetero.pt',
+    hetero_graph_list = torch.load('/data3/data/way_2/twibot22_100w/'+dataset_name+'_subgraphlist_hetero_to_type_1_node_hetero.pt',
                                    map_location=device)
-    raw_graph_list = torch.load('/data3/xupin/0_UNName/data/way_2/twibot22_100w/'+dataset_name+'_subgraphlist.pt',
+    raw_graph_list = torch.load('/data3/data/way_2/twibot22_100w/'+dataset_name+'_subgraphlist.pt',
                                 map_location=device)
         
     val_label_list = []
@@ -356,23 +288,17 @@ if __name__ == '__main__':
     test_label_list = []
     test_pred_list = []
     
-    #! 创建日记记录器
     main_logger = mp_logger(dataset_name+'_way_2_hetero_subgraph_train')
     
-    #! 分别对每个子图进行训练
     for index, subgraph_pair in enumerate(zip(hetero_graph_list, raw_graph_list)):
         hetero_subgraph=subgraph_pair[0]
         raw_subgraph=subgraph_pair[1]
                 
-        #! 训练模型
         print('train hetero subgraph '+str(index+1))
         main_logger.info('train hetero subgraph '+str(index+1))
         
-        #! 创建训练器
         botrgcn_trainer = BotRGCN_Trainer(args, hetero_subgraph, raw_subgraph)
-        #! 对子图进行训练
         val_label, val_pred, test_label, test_pred = botrgcn_trainer.concat_train(logger=main_logger)
-        #! 记录对应值
         val_label_list.append(val_label)
         val_pred_list.append(val_pred)
         test_label_list.append(test_label)
@@ -383,27 +309,17 @@ if __name__ == '__main__':
         print()
         main_logger.info('\n')
 
-    #TODO 是不是要保存 val_label_list, val_pred_list, test_label_list, test_pred_list; 以进行更详细的分析
-    #TODO current_datetime = datetime.now()
-    #TODO formatted_datetime = current_datetime.strftime("%Y_%m_%d_%H_%M_%S")
-    #TODO torch.save(torch.tensor([val_label_list, val_pred_list, test_label_list, test_pred_list]),
-    #TODO            '/data3/xupin/0_UNName/logs/'+ formatted_datetime + '_way_2_hetero_subgraph_label_pred.pt')
-
-    #! 进行合并
     val_label = np.concatenate((val_label_list), axis=0)
     val_pred = np.concatenate((val_pred_list), axis=0)
     test_label = np.concatenate((test_label_list), axis=0)
     test_pred  = np.concatenate((test_pred_list), axis=0)
         
-    #! 验证集准确率
     val_acc = accuracy_score(val_label, val_pred)
-    #! 测试集准确率、F1值、召回率、精确率
     test_acc = accuracy_score(test_label, test_pred)
     test_f1 = f1_score(test_label, test_pred)
     test_recall = recall_score(test_label, test_pred)
     test_precision = precision_score(test_label, test_pred)
 
-    #! 计算混淆矩阵
     tn, fp, fn, tp = confusion_matrix(test_label, test_pred).ravel()
     mcc = matthews_corrcoef(test_label, test_pred) 
     
